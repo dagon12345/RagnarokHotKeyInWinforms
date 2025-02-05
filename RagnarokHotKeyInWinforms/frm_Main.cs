6a04a8901986879585f59bfe83ba18011bf8c7f2
dagon12345 <lanceespina478@gmail.com>
2025-02-05 22:28:54 +0800

    Add project files.

---
+++ RagnarokHotKeyInWinforms/frm_Main.cs
@@ -0,0 +1,44 @@
+﻿using RagnarokHotKeyInWinforms.Utilities;
+using System;
+using System.Windows.Forms;
+
+namespace RagnarokHotKeyInWinforms
+{
+    public partial class frm_Main : Form, IObserver
+    {
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        public void Update(ISubject subject)
+        {
+            switch ((subject as Subject).Message.code)
+            {
+                case MessageCode.TURN_ON:
+                case MessageCode.PROFILE_CHANGED:
+                    Client client = ClientSingleton.GetClient();
+                    if (client != null)
+                    {
+                        characterName.Text = ClientSingleton.GetClient().ReadCharacterName();
+                    }
+                    break;
+                case MessageCode.SERVER_LIST_CHANGED:
+                    this.refreshProcessList();
+                    break;
+                case MessageCode.CLICK_ICON_TRAY:
+                    this.Show();
+                    this.WindowState = FormWindowState.Normal;
+                    break;
+                case MessageCode.SHUTDOWN_APPLICATION:
+                    this.ShutdownApplication();
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

df5b4d696ef851562b17659b4ea9b12fc2c09739
dagon12345 <lanceespina478@gmail.com>
2025-02-08 19:36:00 +0800

    DoneRetrievingGameAddress

---
+++ RagnarokHotKeyInWinforms/Forms/frm_Main.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace RagnarokHotKeyInWinforms
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.characterName = new System.Windows.Forms.Label();
+            this.processCombobox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbSupportedServer = new System.Windows.Forms.ProgressBar();
+            this.brnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Character Name";
+            // 
+            // characterName
+            // 
+            this.characterName.AutoSize = true;
+            this.characterName.Location = new System.Drawing.Point(12, 129);
+            this.characterName.Name = "characterName";
+            this.characterName.Size = new System.Drawing.Size(37, 13);
+            this.characterName.TabIndex = 1;
+            this.characterName.Text = "----------";
+            // 
+            // processCombobox
+            // 
+            this.processCombobox.FormattingEnabled = true;
+            this.processCombobox.Location = new System.Drawing.Point(12, 189);
+            this.processCombobox.Name = "processCombobox";
+            this.processCombobox.Size = new System.Drawing.Size(121, 21);
+            this.processCombobox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ragnarok Client";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Updating Supported Servers...";
+            // 
+            // pbSupportedServer
+            // 
+            this.pbSupportedServer.Location = new System.Drawing.Point(12, 26);
+            this.pbSupportedServer.Name = "pbSupportedServer";
+            this.pbSupportedServer.Size = new System.Drawing.Size(776, 23);
+            this.pbSupportedServer.TabIndex = 4;
+            // 
+            // brnRefresh
+            // 
+            this.brnRefresh.Location = new System.Drawing.Point(139, 189);
+            this.brnRefresh.Name = "brnRefresh";
+            this.brnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.brnRefresh.TabIndex = 6;
+            this.brnRefresh.Text = "Refresh";
+            this.brnRefresh.UseVisualStyleBackColor = true;
+            this.brnRefresh.Click += new System.EventHandler(this.brnRefresh_Click);
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.brnRefresh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbSupportedServer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.processCombobox);
+            this.Controls.Add(this.characterName);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label characterName;
+        private System.Windows.Forms.ComboBox processCombobox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar pbSupportedServer;
+        private System.Windows.Forms.Button brnRefresh;
+    }
+}
+
---
+++ RagnarokHotKeyInWinforms/Forms/frm_Main.cs
@@ -0,0 +1,172 @@
+﻿using Newtonsoft.Json;
+using RagnarokHotKeyInWinforms.Model;
+using RagnarokHotKeyInWinforms.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RagnarokHotKeyInWinforms
+{
+    public partial class frm_Main : Form, IObserver
+    {
+        private Subject subject = new Subject();
+        private Timer progressTimer;
+        private int progressIncrement; // Increment value for each tick
+        private int targetProgress; // Target progress value
+        List<ClientDTO> clients = new List<ClientDTO>();
+        public frm_Main()
+        {
+      
+            this.subject.Attach(this);
+            InitializeComponent();
+
+            this.Text = AppConfig.Name + " - " + AppConfig.Version; // Window title
+            //Container Configuration
+            this.IsMdiContainer = true;
+            SetBackGroundColorOfMDIForm();
+        
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartUpdate();
+            this.refreshProcessList(); // refresh the combobox and get the game
+        }
+        #region Public methods
+        public void Update(ISubject subject)
+        {
+            switch ((subject as Subject).Message.code)
+            {
+                case MessageCode.TURN_ON:
+                case MessageCode.PROFILE_CHANGED:
+                    Client client = ClientSingleton.GetClient();
+                    if (client != null)
+                    {
+                        characterName.Text = ClientSingleton.GetClient().ReadCharacterName();
+                    }
+                    break;
+                case MessageCode.SERVER_LIST_CHANGED:
+                    this.refreshProcessList();
+                    break;
+                case MessageCode.CLICK_ICON_TRAY:
+                    this.Show();
+                    this.WindowState = FormWindowState.Normal;
+                    break;
+                case MessageCode.SHUTDOWN_APPLICATION:
+                    this.ShutdownApplication();
+                    break;
+            }
+        }
+        #endregion
+        #region Private Methods
+        private void StartUpdate()
+        {
+           
+            pbSupportedServer.Value = 0; // Initialize progress bar
+            pbSupportedServer.Maximum = 100; // Set maximum value for progress bar
+
+            //Check the json file named supported_server.json
+            try
+            {
+                //Load the local servers address.
+                clients.AddRange(LocalServerManager.GetLocalClients());
+                //If tech is successful load the resource file locally now API needed
+                //path where the supported_server.json was saved \bin\Debug\Resources
+                string localFilePath = Path.Combine(AppConfig.LocalResourcePath, "supported_servers.json");
+                string localServerRaw = File.ReadAllText(localFilePath);
+                clients.AddRange(JsonConvert.DeserializeObject<List<ClientDTO>>(localServerRaw));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            // Prepare for the progress bar
+            if (clients.Count > 0)
+            {
+                progressIncrement = 100 / clients.Count; // Calculate increment per client
+                targetProgress = 0; // Start from 0
+                progressTimer = new Timer();
+                progressTimer.Interval = 100; // Set timer interval (100 ms)
+                progressTimer.Tick += ProgressTimer_Tick; // Attach tick event handler
+                progressTimer.Start(); // Start the timer
+            }
+            else
+            {
+                // No clients to load, close the form immediately
+                //this.Close();
+            }
+        }
+        private void ProgressTimer_Tick(object sender, EventArgs e)
+        {
+            if (targetProgress < 100)
+            {
+                targetProgress += progressIncrement;
+                if (targetProgress > 100) targetProgress = 100; // Ensure it doesn't exceed 100
+                pbSupportedServer.Value = targetProgress; // Update progress bar value
+            }
+            else
+            {
+                progressTimer.Stop(); // Stop the timer when complete
+                LoadServers(clients); // Call the method to load servers
+            }
+        }
+        private void LoadServers(List<ClientDTO> clients)
+        {
+            foreach (ClientDTO clientDto in clients)
+            {
+                try
+                {
+                    // Scan processes to get our game address.
+                    ClientListSingleton.AddClient(new Client(clientDto));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading client: {ex.Message}");
+                }
+            }
+        }
+
+        private void refreshProcessList()
+        {
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                this.processCombobox.Items.Clear();
+            });
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.MainWindowTitle != "" && ClientListSingleton.ExistsByProcessName(p.ProcessName))
+                {
+                    this.processCombobox.Items.Add(string.Format("{0}.exe - {1}", p.ProcessName, p.Id));
+                }
+            }
+        }
+        private void ShutdownApplication()
+        {
+            KeyboardHook.Disable();
+            subject.Notify(new Utilities.Message(MessageCode.TURN_OFF, null));
+            Environment.Exit(0);
+        }
+        private void SetBackGroundColorOfMDIForm()
+        {
+            foreach (Control ctl in this.Controls)
+            {
+                if ((ctl) is MdiClient)
+                {
+                    ctl.BackColor = Color.White;
+                }
+
+            }
+        }
+        #endregion
+
+        private void brnRefresh_Click(object sender, EventArgs e)
+        {
+            this.refreshProcessList();
+        }
+    }
+}

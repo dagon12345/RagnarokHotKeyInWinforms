a90bed11768e12d05fd15ec2aed7ee7951d5d949
dagon12345 <lanceespina478@gmail.com>
2025-07-04 21:31:43 +0800

    Version2

--- ApplicationLayer/AppConfig.cs
+++ ApplicationLayer/AppConfig.cs
@@ -17,19 +17,22 @@ namespace RagnarokHotKeyInWinforms
         public static SshSettings SshSettings { get; private set; }
         public static DatabaseSettings DatabaseSettings { get; private set; }
 
-        public static void Load(string path = null)
+        public static void Load(string projectRoot = null)
         {
-            if (string.IsNullOrEmpty(path))
+            if (string.IsNullOrEmpty(projectRoot))
             {
                 // Go up three levels to reach the project root from bin/Debug
-                var projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
-                path = Path.Combine(projectRoot, "ApplicationLayer" ,"appsettings.json");
+                //var projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+                //path = Path.Combine(projectRoot, "ApplicationLayer" ,"appsettings.json");
+
+                projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json"));
+               // path = Path.Combine(projectRoot, "ApplicationLayer", "appsettings.json");
             }
 
-            if (!File.Exists(path))
-                throw new FileNotFoundException("Config file not found at: " + path);
+            if (!File.Exists(projectRoot))
+                throw new FileNotFoundException("Config file not found at: " + projectRoot);
 
-            var json = File.ReadAllText(path);
+            var json = File.ReadAllText(projectRoot);
             dynamic parsed = JsonConvert.DeserializeObject(json);
 
             SshSettings = JsonConvert.DeserializeObject<SshSettings>(parsed.Ssh.ToString());
---
+++ Domain/Constants/GoogleConstants.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Constants
+{
+    public class GoogleConstants
+    {
+        [Display(Name = "Client Id")]
+        public const string ClientId = "451216570289-k7khe78035l9d7bd8c3t1oe2pdifhpe4.apps.googleusercontent.com"; // Replace with your Client ID
+        [Display(Name = "Client Secret")]
+        public const string ClientSecret = "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"; // Replace with your Client Secret
+        [Display(Name = "File Name")]
+        public const string TokenFileName = "Google.Apis.Auth.OAuth2.Responses.TokenResponse-user"; // Adjust the file name if needed
+        [Display(Name = "Google Auth Folder")]
+        public const string GoogleApis = "Google.Apis.Auth";
+        [Display(Name = "Google API Url")]
+        public const string GoogleInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
+
+        //Send grid constants
+        public const string SendGridAPI = "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";
+        public const string GoogleEmail = "lanceespina478@gmail.com";
+        public const string AppName = "RagnarokHotkey";
+    }
+
+    public class YahooConstants
+    {
+        public const string YahooMail = "lanceandreiespina@yahoo.com";
+        public const string YahooAppPassword = "gqgbcpkaugdhnyux";
+        public const string YahooSmtp = "smtp.mail.yahoo.com";
+    }
+}

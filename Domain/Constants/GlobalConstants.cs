4b60a9afced656033fb8e775144160fbc6f20604
dagon12345 <lanceespina478@gmail.com>
2025-07-05 20:04:02 +0800

    v17

--- Domain/Constants/GlobalConstants.cs
+++ Domain/Constants/GlobalConstants.cs
@@ -4,7 +4,7 @@
     {
         public const int MaxRetries = 3;
         //Change the version if there are changes along side with the installer.
-        public const string Version = "1.16.0";
+        public const string Version = "1.17.0";
         public const string ZipUrl = "https://www.dropbox.com/scl/fi/ozm2p3ltzbsb1pc8ci32h/FerocityInstaller.zip?rlkey=gyddjf7s17s36w950logs6w04&st=kjccxur9&dl=1";
         public const string VersionUrl = "https://www.dropbox.com/scl/fi/1scpw8x87fmrf7ipqaerw/version.txt?rlkey=05rsq1xlipikyh6q7qtzpavpd&st=0xpj28ye&dl=1";
     }
